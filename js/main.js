4fe40f85c6cef76ea11020cf4ca41c9db8627498
Diem Nguyen <nphdiem95@gmail.com>
2020-08-09 17:26:30 +0700

    Add header and carousel (#1)
    Add header and carousel

--- js/main.js
+++ js/main.js
@@ -1,31 +1,65 @@
-(function(){
-    // Back to Top - by CodyHouse.co
-	var backTop = document.getElementsByClassName('js-cd-top')[0],
-		offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
-		offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		scrollDuration = 700,
-		scrolling = false;
+(function () {
+  // Back to Top - by CodyHouse.co
+  var backTop = document.getElementsByClassName("js-cd-top")[0],
+    offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
+    offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    scrollDuration = 700,
+    scrolling = false;
 
-	if( backTop ) {
-		//update back to top visibility on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
-			}
-		});
+  if (backTop) {
+    //update back to top visibility on scrolling
+    window.addEventListener("scroll", function (event) {
+      if (!scrolling) {
+        scrolling = true;
+        !window.requestAnimationFrame
+          ? setTimeout(checkBackToTop, 250)
+          : window.requestAnimationFrame(checkBackToTop);
+      }
+    });
 
-		//smooth scroll to top
-		backTop.addEventListener('click', function(event) {
-			event.preventDefault();
-			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : Util.scrollTo(0, scrollDuration);
-		});
-	}
+    //smooth scroll to top
+    backTop.addEventListener("click", function (event) {
+      event.preventDefault();
+      !window.requestAnimationFrame
+        ? window.scrollTo(0, 0)
+        : Util.scrollTo(0, scrollDuration);
+    });
+  }
 
-	function checkBackToTop() {
-		var windowTop = window.scrollY || document.documentElement.scrollTop;
-		( windowTop > offset ) ? Util.addClass(backTop, 'cd-top--is-visible') : Util.removeClass(backTop, 'cd-top--is-visible cd-top--fade-out');
-		( windowTop > offsetOpacity ) && Util.addClass(backTop, 'cd-top--fade-out');
-		scrolling = false;
-	}
+  function checkBackToTop() {
+    var windowTop = window.scrollY || document.documentElement.scrollTop;
+    windowTop > offset
+      ? Util.addClass(backTop, "cd-top--is-visible")
+      : Util.removeClass(backTop, "cd-top--is-visible cd-top--fade-out");
+    windowTop > offsetOpacity && Util.addClass(backTop, "cd-top--fade-out");
+    scrolling = false;
+  }
 })();
+
+var btnToggler = document.getElementById("toggler");
+btnToggler.onclick = function () {
+  var nav = document.querySelector(".navbar-expand");
+  if (nav.classList.contains("open")) {
+    nav.classList.remove("open");
+  } else {
+    nav.classList.add("open");
+  }
+};
+var shareElement = document.getElementById("strap__social-share");
+shareElement.onclick = function () {
+  var nav = document.querySelector(".strap__social");
+  if (nav.classList.contains("open")) {
+    nav.classList.remove("open");
+  } else {
+    nav.classList.add("open");
+  }
+};
+
+window.onresize = function () {
+  if (window.innerWidth > 767) {
+    document.querySelector(".strap__social").classList.remove("open");
+  }
+  if (window.innerWidth > 1280) {
+    document.querySelector(".navbar-expand").classList.remove("open");
+  }
+};
